ff9a34453f0feb9151162fd34bc274d709ded130
mocxd <mocxd.a@gmail.com>
2025-04-29 12:55:58 -0400

    sumvalues

---
+++ tree.js
@@ -0,0 +1,48 @@
+/** TreeNode: node for a general tree. */
+
+class TreeNode {
+  constructor(val, children = []) {
+    this.val = val;
+    this.children = children;
+  }
+}
+
+class Tree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** sumValues(): add up all of the values in the tree. */
+
+  sumValues() {
+    let stack = [this.root];
+    let sum = 0;
+
+    while (stack.length) {
+      const current = stack.pop();
+      if (current === null) return 0;
+      sum += current.val;
+      
+      for (let child of current.children) {
+        stack.push(child);
+      }
+    }
+    
+    return sum;
+  }
+
+  /** countEvens(): count all of the nodes in the tree with even values. */
+
+  countEvens() {
+
+  }
+
+  /** numGreater(lowerBound): return a count of the number of nodes
+   * whose value is greater than lowerBound. */
+
+  numGreater(lowerBound) {
+
+  }
+}
+
+module.exports = { Tree, TreeNode };
